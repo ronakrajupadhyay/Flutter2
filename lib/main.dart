e7189c4d267e061e086c5e6faed581e9ad37e636
ronak <ronakupadhyay36@gmail.com>
2025-12-25 12:05:04 +0530

    recipe

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,77 @@
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text('Favorite Button Example')),
+//         body: Center(
+//           child: FavoriteButton(),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// // StatefulWidget
+// class FavoriteButton extends StatefulWidget {
+//   @override
+//   _FavoriteButtonState createState() => _FavoriteButtonState();
+// }
+//
+// class _FavoriteButtonState extends State<FavoriteButton> {
+//   bool isFavorited = false; // State variable
+//
+//   void toggleFavorite() {
+//     setState(() {
+//       isFavorited = !isFavorited; // Toggle state
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return IconButton(
+//       icon: Icon(
+//         isFavorited ? Icons.favorite : Icons.favorite_border,
+//         color: isFavorited ? Colors.red : Colors.grey,
+//         size: 40,
+//       ),
+//       onPressed: toggleFavorite,
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
+import 'recipe_card.dart'; // custom widget file
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text('Favorite Button Example')),
-        body: Center(
-          child: FavoriteButton(),
+        appBar: AppBar(
+          title: const Text("Recipe App"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: RecipeCard(
+            name: "Cheese Pasta",
+            imagePath: "assets/pasta (1).png",
+            time: "30 mins",
+          ),
         ),
       ),
-    );
-  }
-}
-
-// StatefulWidget
-class FavoriteButton extends StatefulWidget {
-  @override
-  _FavoriteButtonState createState() => _FavoriteButtonState();
-}
-
-class _FavoriteButtonState extends State<FavoriteButton> {
-  bool isFavorited = false; // State variable
-
-  void toggleFavorite() {
-    setState(() {
-      isFavorited = !isFavorited; // Toggle state
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        isFavorited ? Icons.favorite : Icons.favorite_border,
-        color: isFavorited ? Colors.red : Colors.grey,
-        size: 40,
-      ),
-      onPressed: toggleFavorite,
     );
   }
 }

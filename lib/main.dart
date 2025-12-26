5f4915226bb136f9600dd001266f9e19d42e77ad
ronak <ronakupadhyay36@gmail.com>
2025-12-26 16:01:49 +0530

    feat: create add recipe form with validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'recipe_card.dart';
-import 'recipe_detail_page.dart';
+import 'add_recipe_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,52 +10,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Recipes App',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const RecipeListPage(),
-        '/detail': (context) => const RecipeDetailPage(),
-      },
+      home: HomePage(),
     );
   }
 }
 
-class RecipeListPage extends StatelessWidget {
-  const RecipeListPage({super.key});
-
-  final List<String> recipes = const [
-    "Paneer Butter Masala",
-    "Veg Biryani",
-    "Masala Dosa",
-    "Chole Bhature",
-    "Rajma Chawal",
-    "Aloo Paratha",
-    "Pav Bhaji",
-    "Fried Rice",
-    "Veg Pulao",
-    "Dal Tadka",
-  ];
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Recipes")),
-      body: ListView.builder(
-        itemCount: recipes.length,
-        itemBuilder: (context, index) {
-          return RecipeCard(
-            recipeName: recipes[index],
-            onTap: () {
-              Navigator.pushNamed(
-                context,
-                '/detail',
-                arguments: recipes[index], // 👈 passing data
-              );
-            },
-          );
-        },
+      appBar: AppBar(title: const Text("Recipes App")),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Add Recipe"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const AddRecipePage(),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
